f1b307905471567b3dffb9ffb6ad9d25bb63d8ad
Vulong112200 <82803163+Vulong112200@users.noreply.github.com>
2021-05-28 21:55:32 +0700

    Delete Admin directory

--- Admin/views/home.php
+++
@@ -1,9 +0,0 @@
-<!-- Page Heading -->
-<div class="row">
-    <div class="col-lg-12">
-        <h1 class="page-header">
-            Hello! <small>Good job</small>
-        </h1>
-    </div>
-</div>
-<!-- /.row -->
